0f48a9e2e706e7d134cbead76425fcb7e9c4f7b2
Carlos Sandoval <iznomag@gmail.com>
2019-06-18 21:08:17 -0500

    Add css cart-item

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./app";
+import Cart from "./components/cart-item";
 
 const $root = document.getElementById("root");
-render(<App />, $root);
+render(<Cart />, $root);
