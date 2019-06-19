f3f3c2f79675dc0b0d910d7773951effc155dd76
byteconde <iamdeyvi@gmail.com>
2019-06-18 20:39:45 -0500

    Get new changes from mates

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-import React from "react";
-import { render } from "react-dom";
-import App from "./app";
-
-const $root = document.getElementById("root");
-render(<App />, $root);
-=======
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ProductDetail from './views/product-detail';
-import { listProduct } from './data/product-fake';
+import { render } from 'react-dom';
+import App from './app';
 
-const product = listProduct[0];
-console.log(product);
-
-ReactDOM.render(
-  <ProductDetail product={product} />,
-  document.getElementById('root')
-);
->>>>>>> Product list
+const $root = document.getElementById('root');
+render(<App />, $root);
